1edc81fe2cc8d38694bd59513f71609f75f90ba7
MagcDEV <manuel117g@gmail.com>
2020-11-20 11:37:24 -0400

    avances

--- init.R
+++ init.R
@@ -7,7 +7,7 @@ url1 <- "http://www.bcv.org.ve/sites/default/files/indicadores_sector_monetario/
 
 data_bcv  <- read.xls(url1, sheet = "LIQUIDEZ_2019-2020", pattern = "al")
 
-data_bcv <- data_bcv[3:length(data_bcv$X.5), c("X.5", "X.6")]
+data_bcv <- data_bcv[, c(7, 8)]
 
 names(data_bcv) <- c("liquidez_monetaria_semanal_bs", "variacion")
 
@@ -65,24 +65,22 @@ tabla_idi$idi <- as.numeric(tabla_idi$idi)
 
 tabla_idi$fecha <- as.Date(tabla_idi$fecha, "%d-%m-%Y")
 
-tabla_idi <- tabla_idi %>%
-    mutate(pct_change = (tipo_de_cambio / lead(tipo_de_cambio) - 1) * 100)
-
 data_bcv <- merge(data_bcv, tabla_idi, all.x = TRUE, by = "fecha")
 
-data_bcv <- data_bcv[!is.na(data_bcv$pct_change), ]
+data_bcv <- data_bcv[!is.na(data_bcv$tipo_de_cambio), ]
 
-data_bcv_1 <- data_bcv[, c("fecha", "variacion")]
-data_bcv_2 <- data_bcv[, c("fecha", "pct_change")]
+data_bcv <- data_bcv %>%
+    mutate(pct_change = (tipo_de_cambio / lag(tipo_de_cambio) - 1) * 100)
 
-names(data_bcv_2) <- c("fecha", "variacion")
-
-data_bcv_1$variable <- 
-
-data_bcv <- rbind(data_bcv_1, data_bcv_2)
-
-ggplot(data_bcv, aes(x = fecha, y = variacion)) +
-    geom_line(aes(y = variacion), color = "darkred", size = 1.5) +
-    geom_line(aes(y = pct_change), color = "steelblue", size = 1.5) +
+ggplot() +
+    geom_line(data = data_bcv, aes(x = fecha, y = variacion,
+                                 color = "Liquidez Monetaria"), size = 1.5) +
+    geom_line(data = data_bcv, aes(x = fecha, y = pct_change,
+                                 color = "Dolar BCV"), size = 1.5) +
     geom_hline(yintercept = 10, size = 1) +
-    geom_hline(yintercept = 0, size = 1)
+    geom_hline(yintercept = 0, size = 1) +
+    ggtitle("Variación % Liquidez Monetaria vs Dolar BCV (hasta el 6/11/2020)") +
+    labs(caption = "Fuente: BCV y calculos propios") +
+    scale_color_manual(values = c("Liquidez Monetaria" = "#065FB4",
+                                  "Dolar BCV" = "#57780B"),
+                                  labs(color = "Variación %"))
